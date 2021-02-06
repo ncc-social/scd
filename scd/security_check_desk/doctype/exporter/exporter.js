884300ea3742ad34d2d83a36bf1d1b7eaaf81bae
ncc-social <social@ncc.gov.gh>
2021-02-06 15:45:54 +0000

    Script to fetch company details

--- scd/security_check_desk/doctype/exporter/exporter.js
+++ scd/security_check_desk/doctype/exporter/exporter.js
@@ -6,3 +6,15 @@ frappe.ui.form.on('Exporter', {
 
 	// }
 });
+
+frappe.ui.form.on("Exporter", "refresh", function(frm) {
+	frappe.ui.form.on("Company Authorisation", {
+			"company": function(frm) {
+			frm.add_fetch("company", "company", "company_name");
+			frm.add_fetch("company", "phone_number", "phone_number");
+			frm.refresh_field("company");
+
+		}
+	});
+
+});
