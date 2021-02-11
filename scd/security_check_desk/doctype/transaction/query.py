d26dd5e5f479c587700b5bf919eda71576f444b6
ncc-social <social@ncc.gov.gh>
2021-02-11 06:33:55 +0000

    Tweak

--- scd/security_check_desk/doctype/transaction/query.py
+++ scd/security_check_desk/doctype/transaction/query.py
@@ -2,26 +2,26 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 
-# @frappe.whitelist()
-# def fetch_business(doctype, txt, searchfield, start, page_lan, filters):
-# 	business = filters.get('business')
-# 	result = frappe.db.sql("""
-# 		SELECT
-# 			a.business_name
-# 		FROM
-# 			`tabBusiness Authorisation` a
-# 		WHERE
-# 			a.parent = '{0}'
-# 		""".format(forwarder))
-# 	return result
-
-
 @frappe.whitelist()
-def fetch_business(doctype, txt, searchfield, start, page_len, filters):
-	cond = "1=1"
-	if filters.get('Buiness Authorisation'):
-		cond += " and business = '{}'".format(filters.get('business'))
-	# if filters.get('inspection_type'):
-	# 	cond += " and inspection_type = '{}'".format(filters.get('inspection_type'))
-	return frappe.db.sql("""select business_name  from `tabBusiness Authorisation` 
-			where  {cond}""".format(cond=cond))
+def fetch_business(doctype, txt, searchfield, start, page_lan, filters):
+	# business = filters.get('business')
+	result = frappe.db.sql("""
+		SELECT
+			a.business_name
+		FROM
+			`tabBusiness Authorisation` a
+		WHERE
+			a.parent = '{0}'
+		""".format(forwarder))
+	return result
+
+
+# @frappe.whitelist()
+# def fetch_business(doctype, txt, searchfield, start, page_len, filters):
+# 	cond = "1=1"
+# 	if filters.get('Buiness Authorisation'):
+# 		cond += " and business = '{}'".format(filters.get('business'))
+# 	# if filters.get('inspection_type'):
+# 	# 	cond += " and inspection_type = '{}'".format(filters.get('inspection_type'))
+# 	return frappe.db.sql("""select business_name  from `tabBusiness Authorisation` 
+# 			where  {cond}""".format(cond=cond))
