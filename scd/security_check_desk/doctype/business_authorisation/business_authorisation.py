0368f753f299a646cce4d0355d254a4537d1b05f
ncc-social <social@ncc.gov.gh>
2021-02-14 11:36:48 +0000

    business authorisation autoname

--- scd/security_check_desk/doctype/business_authorisation/business_authorisation.py
+++ scd/security_check_desk/doctype/business_authorisation/business_authorisation.py
@@ -2,9 +2,13 @@
 # Copyright (c) 2021, NCC and contributors
 # For license information, please see license.txt
 
-from __future__ import unicode_literals
-# import frappe
+import frappe, re
 from frappe.model.document import Document
+from frappe.model.naming import make_autoname
 
 class BusinessAuthorisation(Document):
-	pass
+	def autoname(self):
+		prefix = re.sub('-+', '-', self.business_name.replace(' ', '-'))
+		self.name = make_autoname(prefix + '(-.##)')
+
+
