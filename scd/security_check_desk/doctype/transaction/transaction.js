95f58b2585e8ddf274d08804656ef8023546917c
ncc-social <social@ncc.gov.gh>
2021-02-18 06:25:02 +0000

    Tweaks

--- scd/security_check_desk/doctype/transaction/transaction.js
+++ scd/security_check_desk/doctype/transaction/transaction.js
@@ -49,7 +49,7 @@ frappe.ui.form.on(cur_frm.doctype, {
 });
 // Link formatter sample from Employee
 frappe.form.link_formatters['Business Authorisation'] = function(value, doc) {
-    if(doc.business && doc.business !== value) {
+    if(doc.business_name && doc.business_name !== value) {
         return value + ':/ ' + doc.business_name;
     } else {
         return value;
