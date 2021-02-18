a878f31873931b762436b5119a5f320f8d32cc26
ncc-social <social@ncc.gov.gh>
2021-02-18 09:03:08 +0000

    Tweak

--- scd/security_check_desk/doctype/exporter/custom.py
+++ scd/security_check_desk/doctype/exporter/custom.py
@@ -1,9 +1,9 @@
 from __future__ import unicode_literals
 import frappe
-from frappe.model.document import Document
+from frappe import _
+from frappe.utils import flt, cstr
 from frappe.model.mapper import get_mapped_doc
-from frappe.utils import getdate, validate_email_address, today, add_years, format_datetime, cstr
-from frappe import throw, _, scrub
+from frappe.model.document import Document
 
 @frappe.whitelist()
 def create_forwarder(docname):
