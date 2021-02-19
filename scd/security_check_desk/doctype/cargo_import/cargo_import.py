0aeaaad7a851c56a8466495a75250d82514918a1
ncc-social <social@ncc.gov.gh>
2021-02-19 13:07:41 +0000

    Tweak

---
+++ scd/security_check_desk/doctype/cargo_import/cargo_import.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021, NCC and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+# cargo_import frappe
+from frappe.model.document import Document
+
+class CargoImport(Document):
+	pass
