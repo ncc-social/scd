e7cf5dad2dd6198d5704871e2a90625aaa9e9f07
ncc-social <social@ncc.gov.gh>
2021-02-20 15:58:30 +0000

    Tweaks

---
+++ scd/security_check_desk/doctype/officer/custom.py
@@ -0,0 +1,35 @@
+from __future__ import unicode_literals
+import frappe
+from frappe import _
+from frappe.utils import flt, cstr
+from frappe.model.mapper import get_mapped_doc
+from frappe.model.document import Document
+
+@frappe.whitelist()
+def create_user(docname):
+    forwarder_exist = frappe.db.exists("Forwarder", {"exporter_record": docname})
+    if forwarder_exist:
+        frappe.throw(_("Exporter already exists as a Forwarder"))
+
+    exporter_info = frappe.get_doc("Exporter", docname)
+    new_forwarder = frappe.new_doc("Forwarder")
+    new_forwarder.first_name = exporter_info.first_name
+    new_forwarder.middle_name = exporter_info.middle_name
+    new_forwarder.last_name = exporter_info.last_name
+    new_forwarder.forwarder_photo = exporter_info.exporter_photo
+    new_forwarder.exporter_record = exporter_info.name
+    new_forwarder.date_of_birth = exporter_info.date_of_birth
+    new_forwarder.place_of_birth = exporter_info.place_of_birth
+    new_forwarder.region_place_of_birth = exporter_info.region_place_of_birth
+    new_forwarder.hometown = exporter_info.hometown
+    new_forwarder.region_hometown = exporter_info.region_hometown
+    new_forwarder.residential_address = exporter_info.residential_address
+    new_forwarder.popular_landmark = exporter_info.popular_landmark
+    new_forwarder.postal_address = exporter_info.postal_address
+    new_forwarder.phone_number = exporter_info.phone_number
+    new_forwarder.personal_email = exporter_info.personal_email
+    new_forwarder.id_type = exporter_info.id_type
+    new_forwarder.id_number = exporter_info.id_number
+    new_forwarder.photo_id = exporter_info.photo_id
+    new_forwarder.fingerprint_impressions = exporter_info.fingerprint_impressions
+    return new_forwarder.as_dict()
---
+++ scd/security_check_desk/doctype/officer/officer.js
@@ -0,0 +1,31 @@
+// Copyright (c) 2021, NCC and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Officer', {
+	// refresh: function(frm) {
+
+	// }
+});
+frappe.ui.form.on("Officer", {
+    refresh: function(frm) {
+        if(frm.doc.docstatus == 0) {
+            frm.add_custom_button(__('As User'), function() {
+                frm.events.create_user(frm);
+            }, __('Create'));
+            frm.page.set_inner_btn_group_as_primary(__('Create'));
+        }
+    },
+
+    create_forwarder: function(frm){
+        frappe.call({
+            method: "scd.security_check_desk.doctype.officer.custom.create_user",
+            args:{
+                docname: frm.doc.name
+            },
+            callback: function(r){
+                var doc = frappe.model.sync(r.message);
+                frappe.set_route('Form', 'Forwarder', r.message.name);
+            }
+        });
+    }
+});
