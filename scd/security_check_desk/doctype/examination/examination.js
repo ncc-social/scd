72fe1d47dd840ba84dd440b1204ec658e5656454
ncc-social <social@ncc.gov.gh>
2021-02-20 21:16:48 +0000

    e

--- scd/security_check_desk/doctype/examination/examination.js
+++ scd/security_check_desk/doctype/examination/examination.js
@@ -2,7 +2,27 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Examination', {
-	// refresh: function(frm) {
+	refresh: function(frm) {
+		frm.set_query("examination_officer", function(frm, cdt, cdn) {
+			// var a = locals[cdt][cdn];
+			var d = new Date;
+			return {
+				filters: [
+					["designation", "=", "Officer"],
+				]
+			}
+		});
+	}
+});
 
-	// }
+frappe.ui.form.on('Examination', {
+	refresh: function(frm) {
+		frm.add_fetch('officer','officer_name','officer_name');
+		// frm.add_fetch('exporter','phone_number','phone_number');
+		// frm.add_fetch('exporter','exporter_photo','exporter_photo');
+		// frm.add_fetch('forwarder','forwarder_name','forwarder_name');
+		// frm.add_fetch('consignee','consignee_name','consignee_name');
+		// frm.add_fetch('consignee','consignee_country','consignee_country');
+		// frm.add_fetch('business','business_name','business_name');
+	}
 });
