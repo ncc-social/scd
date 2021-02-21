e357e05ffbed393094e7764315b50567e79627c2
ncc-social <social@ncc.gov.gh>
2021-02-21 08:12:18 +0000

    s

--- scd/security_check_desk/doctype/examination/examination.js
+++ scd/security_check_desk/doctype/examination/examination.js
@@ -26,6 +26,7 @@ frappe.ui.form.on('Examination', {
 		frm.add_fetch('cargo_import','weight','weight');
 		frm.add_fetch('cargo_import','description','description');
 		frm.add_fetch('cargo_import','arrival_date','arrival_date');
+		frm.add_fetch('cargo_import','description','description');
 		frm.add_fetch('officer','officer_name','officer_name');
 		frm.add_fetch('supervisor','supervisor_name','supervisor_name');
 		frm.add_fetch('forwarder','forwarder_name','forwarder_name');
