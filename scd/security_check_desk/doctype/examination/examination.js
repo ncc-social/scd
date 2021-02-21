cdebbdc41f5809d6c131878bc50af92add4b42e5
ncc-social <social@ncc.gov.gh>
2021-02-21 00:18:52 +0000

    g

--- scd/security_check_desk/doctype/examination/examination.js
+++ scd/security_check_desk/doctype/examination/examination.js
@@ -7,7 +7,7 @@ frappe.ui.form.on('Examination', {
 
 frappe.ui.form.on('Examination', {
 	refresh: function(frm) {
-		frm.set_query("import", function(frm, cdt, cdn) {
+		frm.set_query("cargo_import", function(frm, cdt, cdn) {
 			return {
 				filters: [
 					["status", "=", "Tagged"]
@@ -20,17 +20,19 @@ frappe.ui.form.on('Examination', {
 
 frappe.ui.form.on('Examination', {
 	refresh: function(frm) {
-		frm.add_fetch('officer','officer_name','officer_name');
-		frm.add_fetch('supervisor','supervisor_name','supervisor_name');
+		
 		frm.add_fetch('Cargo Import','flight_number','flight_number');
 		frm.add_fetch('Cargo Import','flight_date','flight_date');
 		frm.add_fetch('Cargo Import','awb','awb');
 		frm.add_fetch('Cargo Import','weight','weight');
 		frm.add_fetch('Cargo Import','description','description');
 		frm.add_fetch('Cargo Import','arrival_date','arrival_date');
+		frm.add_fetch('officer','officer_name','officer_name');
+		frm.add_fetch('supervisor','supervisor_name','supervisor_name');
+		frm.add_fetch('forwarder','forwarder_name','forwarder_name');
+		frm.add_fetch('consignee','consignee_name','consignee_name');
 		// frm.add_fetch('exporter','phone_number','phone_number');
 		// frm.add_fetch('exporter','exporter_photo','exporter_photo');
-		frm.add_fetch('forwarder','forwarder_name','forwarder_name');
 		// frm.add_fetch('consignee','consignee_name','consignee_name');
 		// frm.add_fetch('consignee','consignee_country','consignee_country');
 		// frm.add_fetch('business','business_name','business_name');
