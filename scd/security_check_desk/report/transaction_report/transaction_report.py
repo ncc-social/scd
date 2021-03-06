c9c1fe4757c6053b1ea39e57fa9ee714523a1ca3
ncc-social <social@ncc.gov.gh>
2021-03-06 14:05:18 +0000

    s

--- scd/security_check_desk/report/transaction_report/transaction_report.py
+++ scd/security_check_desk/report/transaction_report/transaction_report.py
@@ -83,7 +83,7 @@ def get_conditions(filters) :
 	conditions = []
 
 	if filters.get("exporter_name"):
-		conditions.append(" and exporter_name LIKE %(exporter_name)s")
+		conditions.append(" and exporter_name LIKE %%(exporter_name)s%")
     
 	if filters.get("forwarder_name"):
 		conditions.append(" and exporter_name LIKE %(forwarder_name)s")
