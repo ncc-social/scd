36c100f4146c6c71c227e8ff615c58d596edec4a
ncc-social <social@ncc.gov.gh>
2021-03-06 13:11:16 +0000

    h

--- scd/security_check_desk/report/transaction_report/transaction_report.py
+++ scd/security_check_desk/report/transaction_report/transaction_report.py
@@ -1,70 +1,94 @@
-# Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
-# License: GNU General Public License v3. See license.txt
+# Copyright (c) 2020, Frappe Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
 
 from __future__ import unicode_literals
-import frappe
 from frappe import _
-from frappe.utils import flt
+import frappe
 
 def execute(filters=None):
-	if not filters: filters = {}
-
-	columns = get_columns()
-	data = get_transactions(filters)
-
+	columns, data = get_columns(), get_data(filters)
 	return columns, data
 
 def get_columns():
-	return [
-		_("Transaction Date") + ":Date:100",
-        _("Exporter") + ":Data:200",
-        _("Forwarder") + ":Data:200",
-        _("Consignee") + ":Data:200",
-        _("Quantity") + ":Data:80",
-        _("Weight")+ ":Float:100",
-		_("Cargo Description") + ":Data:200",
-        _("Country") + ":Data:100"
+	columns = [
+		{
+			"label": _("Transaction Date"),
+			"fieldname": "date_of_transaction",
+			"fieldtype": "Date",
+			"width": 100,
+		},
+		{
+			"label": _("Exporter"),
+			"fieldname": "exporter_name",
+			"fieldtype": "Data",
+			"width": 200
+		},
+        {
+			"label": _("Forwarder"),
+			"fieldname": "forwarder_name",
+			"fieldtype": "Data",
+			"width": 200
+		},
+        {
+			"label": _("Consignee"),
+			"fieldname": "consignee_name",
+			"fieldtype": "Data",
+			"width": 200
+		},
+		{
+			"fieldname":"quantity",
+			"label": _("Quantity"),
+			"fieldtype": "Int",
+			"width": 80
+		},
+		{
+			"fieldname":"weight",
+			"label": _("Weight"),
+			"fieldtype": "Float",
+			"width": 100
+		},
+		{
+			"label": _("Cargo Description"),
+			"fieldname": "cargo_description",
+			"fieldtype": "Data",
+			"width": 200
+		},
+		{
+			"label": _("Country"),
+			"fieldname": "country",
+			"fieldtype": "Data",
+			"width": 100
+		}
+		
 	]
+	return columns
 
-    # columns = [
-    #     _("Transaction") + “:date:90”,
-    #     _(“Remarks”) + “::200”,
-    #     _(“Against Account”) + “::200”,
-    #     _(“Amount”) + “:Float:100”,
-    # ]
-    # if filters.get("exporter"):
-    #     columns += [
-    #         _("Forwarder") + ":Data:200",
-    #         _("Consignee")+ ":Data:100"
-    #     ]
-    #     columns += [
-    #         _(“Voucher Type”) + “::120”
-    #     ]
-    # return columns
+def get_data(filters):
+	return frappe.db.sql("""
+		SELECT
+			date_of_transaction,
+			exporter_name,
+			`forwarder_name,
+			consignee_name,
+			quantity,
+			weight,
+			cargo_description,
+			consignee_country
+		FROM
+			tabTransaction (
+		WHERE
+			1 = 1 {conditions}""".format(conditions=get_conditions(filters)), filters, as_dict=1)
 
-def get_transactions(filters):
-	conditions = get_conditions(filters)
-	return frappe.db.sql("""select date_of_transaction, exporter_name, forwarder_name, consignee_name,
-	quantity, weight, cargo_description,
-	consignee_country from tabTransaction where 1 = 1 %s""" % conditions, as_list=1)
+def get_conditions(filters) :
+	conditions = []
 
-def get_conditions(filters):
-	conditions = ""
 	if filters.get("exporter_name"):
-		conditions += " and exporter_name LIKE '%%s%%'" % filters["exporter_name"]
-	
-	if filters.get("forwarder_name"):
-		conditions += " and forwarder_name LIKE '%%s%%'" % filters["forwarder_name"]
+		conditions.append(" and and exporter_name LIKE %(exporter_name)s")
+    
+    if filters.get("forwarder_name"):
+		conditions.append(" and and exporter_name LIKE %(forwarder_name)s")
 
-	if filters.get("consignee_name"):
-		conditions += " and consignee_name LIKE '%%s%%'" % filters["consignee_name"]
-	
-	# if filters.get("month"):
-	# 	month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov",
-	# 		"Dec"].index(filters["month"]) + 1
-	# 	conditions += " and month(date_of_birth) = '%s'" % month
+    if filters.get("consignee_name"):
+		conditions.append(" and and exporter_name LIKE %(consignee_name)s")
 
-	# if filters.get("company"): conditions += " and company = '%s'" % \
-	# 	filters["company"].replace("'", "\\'")
-
-	return conditions
+	return " ".join(conditions) if conditions else ""
