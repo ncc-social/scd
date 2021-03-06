8cdfe6a661ee7d33271c72d43643fb7fa4815655
ncc-social <social@ncc.gov.gh>
2021-03-06 20:01:20 +0000

    j

--- scd/security_check_desk/doctype/forwarder/forwarder.js
+++ scd/security_check_desk/doctype/forwarder/forwarder.js
@@ -12,3 +12,18 @@ frappe.ui.form.on("Forwarder", "refresh", function(frm) {
 	});
 
 });
+
+frappe.ui.form.on(cur_frm.doctype, {
+    refresh: frm => {
+        if (frm.doc.photo_id) {
+            frm.set_df_property("view_photo_id", "options", `
+                <object width="300" height="400" data="${ frm.doc.photo_id }"></object>
+            `);
+        }
+		if (frm.doc.fingerprint_impressions) {
+            frm.set_df_property("view_fingerprint", "options", `
+                <object width="300" height="400" data="${ frm.doc.fingerprint_impressions }"></object>
+            `);
+        }
+    }   
+});
