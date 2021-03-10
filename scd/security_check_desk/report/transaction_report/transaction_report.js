f34be370d868a58241e867f5a599a7dd599d33a6
ncc-social <social@ncc.gov.gh>
2021-03-10 04:38:34 +0000

    h

---
+++ scd/security_check_desk/report/transaction_report/transaction_report.js
@@ -0,0 +1,26 @@
+// Copyright (c) 2016, NCC and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Transaction"] = {
+	"filters": [
+		{
+			"fieldname":"exporter",
+			"label": __("Exporter"),
+			"fieldtype": "Link",
+			"options": "Exporter"
+		},
+        {
+			"fieldname":"forwarder",
+			"label": __("Forwarder"),
+			"fieldtype": "Link",
+			"options": "Forwarder"
+		},
+        {
+			"fieldname":"consignee",
+			"label": __("Consignee"),
+			"fieldtype": "Link",
+			"options": "Consignee"
+		}
+	]
+}
---
+++ scd/security_check_desk/report/transaction_report/transaction_report.py
@@ -0,0 +1,89 @@
+# Copyright (c) 2013, NCC and contributors
+# For license information, please see license.txt
+
+# from __future__ import unicode_literals
+# # import frappe
+
+# def execute(filters=None):
+# 	columns, data = [], []
+# 	return columns, data
+
+from __future__ import unicode_literals
+import frappe
+from frappe import _
+from frappe.utils import flt, cint
+
+def execute(filters=None):
+	if not filters: filters = {}
+
+	columns = get_columns()
+	data = get_transactions(filters)
+
+	return columns, data
+
+def get_columns():
+	return [
+		_("Transaction Date") + ":Date:100",
+        _("Exporter") + ":Data:200",
+        _("Forwarder") + ":Data:200",
+        _("Consignee") + ":Data:200",
+        _("Quantity") + ":Data:80",
+        _("Weight")+ ":Float:100",
+		_("Cargo Description") + ":Data:200",
+        _("Country") + ":Data:100"
+	]
+
+    # columns = [
+    #     _("Transaction") + “:date:90”,
+    #     _(“Remarks”) + “::200”,
+    #     _(“Against Account”) + “::200”,
+    #     _(“Amount”) + “:Float:100”,
+    # ]
+    # if filters.get("exporter"):
+    #     columns += [
+    #         _("Forwarder") + ":Data:200",
+    #         _("Consignee")+ ":Data:100"
+    #     ]
+    #     columns += [
+    #         _(“Voucher Type”) + “::120”
+    #     ]
+    # return columns
+
+def get_transactions(filters):
+	conditions = get_conditions(filters)
+	return frappe.db.sql("""select name, date_of_transaction, exporter_name, forwarder_name, consignee_name,
+	quantity, weight, cargo_description,
+	consignee_country from tabTransaction where 1 = 1 %s""" % conditions, as_list=1)
+
+
+def get_conditions(filters):
+	conditions = ""
+	if filters.get("exporter_name"):
+		conditions += " and exporter_name LIKE '%%s%%'" % filters["exporter_name"]
+	
+	if filters.get("forwarder_name"):
+		conditions += " and forwarder_name LIKE '%%s%%'" % filters["forwarder_name"]
+
+	if filters.get("consignee_name"):
+		conditions += " and consignee_name LIKE '%%s%%'" % filters["consignee_name"]
+	
+# def get_conditions(filters) :
+# 	conditions = []
+
+# 	if filters.get("territory"):
+# 		conditions.append(" and `tabLead`.territory=%(territory)s")
+
+# 	if filters.get("status"):
+# 		conditions.append(" and `tabLead`.status=%(status)s")
+	
+# 	return " ".join(conditions) if conditions else ""
+	
+	# if filters.get("month"):
+	# 	month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov",
+	# 		"Dec"].index(filters["month"]) + 1
+	# 	conditions += " and month(date_of_birth) = '%s'" % month
+
+	# if filters.get("company"): conditions += " and company = '%s'" % \
+	# 	filters["company"].replace("'", "\\'")
+
+	return conditions
