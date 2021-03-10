1be0a82f8369782f8116871063eb8c9ff45b5d36
ncc-social <social@ncc.gov.gh>
2021-03-10 04:53:24 +0000

    g

--- scd/security_check_desk/report/transaction_report/transaction_report.py
+++ scd/security_check_desk/report/transaction_report/transaction_report.py
@@ -54,7 +54,7 @@ def get_transactions(filters):
 	conditions = get_conditions(filters)
 	return frappe.db.sql("""select name, date_of_transaction, exporter_name, forwarder_name, consignee_name,
 	quantity, weight, cargo_description,
-	consignee_country from tabTransaction where 1 = 1 %s""" % conditions, as_list=1)
+	consignee_country from tabTransaction where 1 = 1 %s""" % conditions, filters)
 
 
 def get_conditions(filters):
