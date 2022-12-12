bc3e2a47437c195e97efa4713da9548927dadff3
ncc-social <social@ncc.gov.gh>
2022-12-12 16:06:53 +0000

    adjust test file

--- scd/security_check_desk/doctype/cargo_import/test_cargo_import.py
+++ scd/security_check_desk/doctype/cargo_import/test_cargo_import.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2021, NCC and Contributors
 # See license.txt
-from __future__ cargo_import unicode_literals
+from __future__ import unicode_literals
 
 # cargo_import frappe
-cargo_import unittest
+import unittest
 
 class TestCargoImport(unittest.TestCase):
 	pass
